54470622357d3e1c1db18ada77729194a748dc8b
Fernando <fsanchezrequ@protonmail.com>
2025-10-25 10:20:27 +0200

    [Feat] Added support Page for User

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use LaravelSupportCenter\Http\Controllers\SupportController;
 
 Route::get('/'. config('support-center.route-name'), [SupportController::class, 'support'])->name('support');
 
-Route::middleware(['web', 'auth'])->prefix('support')->name('support.')->group(function () {
-        Route::get('/', [SupportController::class, 'index'])->name('index');
-        Route::post('/', [SupportController::class, 'store'])->name('store');
-    });
+//Route::middleware(['web', 'auth'])->prefix('support')->name('support.')->group(function () {
+//        Route::get('/', [SupportController::class, 'index'])->name('index');
+//        Route::post('/', [SupportController::class, 'store'])->name('store');
+//    });
