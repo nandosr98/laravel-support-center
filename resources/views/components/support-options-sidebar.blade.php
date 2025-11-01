82337cf4fb53d416569b30bd3a2f81737cf50eae
Fernando <fsanchezrequ@protonmail.com>
2025-11-01 11:13:09 +0100

    [Feat] Modified Routes in the components

--- resources/views/components/support-options-sidebar.blade.php
+++ resources/views/components/support-options-sidebar.blade.php
@@ -1,6 +1,6 @@
 <x-mary-menu-sub title="Soporte">
-    <x-mary-menu-item title="Tickets" link="{{ route('support.admin-page', ['section' => 'tickets']) }}" no-wire-navigate/>
-    <x-mary-menu-item title="Categorías" link="{{ route('support.admin-page', ['section' => 'categorias']) }}" no-wire-navigate/>
-    <x-mary-menu-item title="Etiquetas" link="{{ route('support.admin-page', ['section' => 'etiquetas']) }}" no-wire-navigate/>
-    <x-mary-menu-item title="Agentes" link="{{ route('support.admin-page', ['section' => 'agentes']) }}" no-wire-navigate/>
+    <x-mary-menu-item title="Tickets" link="{{ route('support.admin-page.tickets') }}" no-wire-navigate/>
+    <x-mary-menu-item title="Categorías" link="{{ route('support.admin-page.categories') }}" no-wire-navigate/>
+    <x-mary-menu-item title="Etiquetas" link="{{ route('support.admin-page.tags') }}" no-wire-navigate/>
+    <x-mary-menu-item title="Agentes" link="{{ route('support.admin-page.agents') }}" no-wire-navigate/>
 </x-mary-menu-sub>
